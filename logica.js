e842d5492dee21629554711a2924e20a83fc92ca
fernajapb <fernaaguilarpau@gmail.com>
2026-06-13 11:59:24 -0500

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,9 @@
+/*
+const botones = document.querySelectorAll("button");
+
+botones.forEach(boton=>{
+    boton.addEventListener("click",()=>{
+        alert("Próximamente disponible.");
+    });
+});
+*/
